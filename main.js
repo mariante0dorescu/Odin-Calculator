d093bb087cc599da317bb79a367810514c3cc38d
mariante0dorescu <broinkus@gmail.com>
2023-05-26 20:24:09 +0300

    first attempt failed

--- main.js
+++ main.js
@@ -4,28 +4,52 @@ const topField = document.querySelector(".calculator__output--top");
 const bottomField = document.querySelector(".calculator__output--bottom");
 const calcInput = document.querySelector(".calculator__input");
 let operator;
-let x = bottomField.innerText || 0;
-let y = 0;
+let x = "";
+let y;
 
-function calculator(e) {
-  console.log(e.key)
-  
+function calculator(input) {
+  // console.log(input)
 
-  if(e.key >= 0 && e.key <= 9){
-    x += e.key;
-    bottomField.innerText += e.key;
+  if(input === "Escape" || input === "AC") {
+    topField.innerHTML = "&nbsp;";
+    bottomField.innerHTML = "&nbsp;";
+    x = y = 0
   }
 
-  if(e.key === "+") {
-    x = 0;
-    y = Number(bottomField.innerText);
-    operator = e.key;
-    topField.innerText = y;
+  if(input >= 0 && input <= 9){
+    if(bottomField.innerText === "0") {bottomField.innerText=""}
+    x += input;
+    bottomField.innerText += input;
   }
-  if(e.key === "Enter"){
-    //bottomField.innerText = x + y;
-    return
+
+  if(input === "+" || input === "-" || input === "*" || input === "/" || input === "%"){
+    bottomField.innerText = "0"
+    operator = input;
+    y = x;
+    topField.innerText = y + " " + operator;
+  }
+
+  if(input === "Enter" || input === "="){
+    topField.innerHTML = "&nbsp;";
+    bottomField.innerHTML = "&nbsp;";
+    x = calculate(operator, +x, +y);
+    bottomField.innerText = x;
+    //console.log(calculate(operator, x, y))
+    return;
   }
 }
 
-document.addEventListener('keydown', calculator)
+function calculate(operator, x, y){
+ console.log(x,y)
+  bottomField.innerHTML = "&nbsp;";
+  if(operator === "+") return Number(x) + Number(y);
+  if(operator === "-") return x - y;
+  if(operator === "/") return y / x;
+  if(operator === "*") return x * y;
+  if(operator === "%") return (x / y) * 100;
+}
+
+document.addEventListener('keydown', (e) => calculator(e.key))
+calcInput.addEventListener("click", (e) => {
+  calculator(e.target.id)
+})
