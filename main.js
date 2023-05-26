df455e4bf9ef2ee7228ba50e21f9f3e3af7ee8d1
mariante0dorescu <broinkus@gmail.com>
2023-05-26 22:57:01 +0300

    second version: kinda working

--- main.js
+++ main.js
@@ -3,53 +3,81 @@
 const topField = document.querySelector(".calculator__output--top");
 const bottomField = document.querySelector(".calculator__output--bottom");
 const calcInput = document.querySelector(".calculator__input");
+
+let firstNumber ;
+let secondNumber;
 let operator;
-let x = "";
-let y;
 
-function calculator(input) {
-  // console.log(input)
-
-  if(input === "Escape" || input === "AC") {
-    topField.innerHTML = "&nbsp;";
-    bottomField.innerHTML = "&nbsp;";
-    x = y = 0
-  }
-
-  if(input >= 0 && input <= 9){
-    if(bottomField.innerText === "0") {bottomField.innerText=""}
-    x += input;
-    bottomField.innerText += input;
-  }
-
-  if(input === "+" || input === "-" || input === "*" || input === "/" || input === "%"){
-    bottomField.innerText = "0"
-    operator = input;
-    y = x;
-    topField.innerText = y + " " + operator;
-  }
-
-  if(input === "Enter" || input === "="){
-    topField.innerHTML = "&nbsp;";
-    bottomField.innerHTML = "&nbsp;";
-    x = calculate(operator, +x, +y);
-    bottomField.innerText = x;
-    //console.log(calculate(operator, x, y))
-    return;
-  }
+function showResult(result){
+  topField.innerHTML = "&nbsp;";
+  bottomField.innerText = result;
 }
 
-function calculate(operator, x, y){
- console.log(x,y)
-  bottomField.innerHTML = "&nbsp;";
-  if(operator === "+") return Number(x) + Number(y);
-  if(operator === "-") return x - y;
-  if(operator === "/") return y / x;
-  if(operator === "*") return x * y;
-  if(operator === "%") return (x / y) * 100;
+function calculate(n1, operator, n2) {
+  let result = ''
+  
+  if (operator === '+') {
+    result = parseFloat(n1) + parseFloat(n2);
+  } else if (operator === '-') {
+    result = parseFloat(n1) - parseFloat(n2);
+  } else if (operator === '*') {
+    result = parseFloat(n1) * parseFloat(n2);
+  } else if (operator === '/') {
+    result = parseFloat(n1) / parseFloat(n2);
+  } else if (operator === '%') {
+    result = (parseFloat(n1) / parseFloat(n2)) * 100;
+  }
+  
+  return result
 }
 
-document.addEventListener('keydown', (e) => calculator(e.key))
+
+//document.addEventListener('keydown', (e) => console.log(e.key) )
+
 calcInput.addEventListener("click", (e) => {
-  calculator(e.target.id)
+  
+  
+  if(e.target.matches('button')) {//
+    const action = e.target.dataset.operator;
+    if(!action){
+      
+      //console.log(e.target.dataset.key)
+      bottomField.innerText += e.target.dataset.key;
+    }
+
+    if(action) {
+      //console.log(action)
+
+
+      if(action === "DEL" && bottomField.innerHTML !=="&nbsp;") bottomField.innerText = bottomField.innerText.slice(0,-1) 
+      if(action === "AC") bottomField.innerHTML = topField.innerHTML = "&nbsp;";
+      if( bottomField.innerHTML !=="&nbsp;") firstNumber = bottomField.innerText;
+      if(action === "+" || action === "-" || action === "/" || action === "*" || action === "%"){
+        operator = action;
+        topField.innerText = bottomField.textContent + " " + action;
+        bottomField.innerHTML = "&nbsp;"
+        return;
+        // if(firstNumber < 0) {
+        //   console.log('here')
+        //   firstNumber = parseFloat(bottomField.innerText);
+        //   topField.innerText = firstNumber + " " + action;
+        //   bottomField.innerHTML = 0
+        // } 
+        // if(firstNumber > 0){
+        //   console.log('not here')
+        //   secondNumber = parseFloat(bottomField.innerText);
+        //   console.log(firstNumber, operator, secondNumber)
+        //   //bottomField.innerHTML = calculate(firstNumber, operator, secondNumber)
+        //   topField.innerHTML = "&nbsp;"
+        // }
+      }
+      if(action === "result"){
+        bottomField.innerText =  calculate(topField.textContent, operator, bottomField.textContent);
+        topField.innerHTML = "&nbsp;"
+      }
+    }
+  }
+
+  //console.log(e.target.dataset.operator || e.target.dataset.key)
 })
+
